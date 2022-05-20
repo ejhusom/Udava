97fd1ad06e41a10db0d38f79cc743268191471d8
ejhusom <ejlgkvam@online.no>
2022-05-20 08:58:38 +0200

    Update fingerprint to feature_vector

--- src/clustermodel.py
+++ src/clustermodel.py
@@ -84,16 +84,16 @@ class ClusterModel:
         overlap = params["featurize"]["overlap"]
 
         featurized_df = featurize(inference=True, inference_df=inference_df)
-        fingerprint_timestamps = featurized_df.index
+        feature_vector_timestamps = featurized_df.index
 
-        fingerprints = featurized_df.to_numpy()
+        feature_vectors = featurized_df.to_numpy()
         input_scaler = joblib.load(INPUT_SCALER_PATH)
-        fingerprints = input_scaler.transform(fingerprints)
+        feature_vectors = input_scaler.transform(feature_vectors)
 
         model = joblib.load(MODELS_FILE_PATH)
-        labels = model.predict(fingerprints)
+        labels = model.predict(feature_vectors)
         distance_to_centers, sum_distance_to_centers = calculate_distances(
-            fingerprints, model
+            feature_vectors, model
         )
 
         # plt.figure()
@@ -101,11 +101,11 @@ class ClusterModel:
         # plt.show()
 
         if plot_results:
-            # visualize_clusters(labels, fingerprints, model)
+            # visualize_clusters(labels, feature_vectors, model)
             fig_div = plot_labels_over_time(
-                fingerprint_timestamps, labels, fingerprints, inference_df, model
+                feature_vector_timestamps, labels, feature_vectors, inference_df, model
             )
-            # plot_cluster_center_distance(fingerprint_timestamps, fingerprints, model)
+            # plot_cluster_center_distance(feature_vector_timestamps, feature_vectors, model)
             return fig_div
         else:
-            return fingerprint_timestamps, labels, sum_distance_to_centers
+            return feature_vector_timestamps, labels, sum_distance_to_centers
